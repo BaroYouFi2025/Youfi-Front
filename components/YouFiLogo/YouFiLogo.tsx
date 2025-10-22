bd27c65a0fa0f1a98dab97a62f12836bef09e80c
Stephan Kim <hhkimstar1226@gmail.com>
2025-10-23 08:20:12 +0900

    feat: YouFiLogo 컴포넌트 리팩토링 및 불필요한 코드 제거

--- components/YouFiLogo/YouFiLogo.tsx
+++ components/YouFiLogo/YouFiLogo.tsx
@@ -1,67 +1,15 @@
 import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
 import {
   LogoContainer,
-  LogoSquareContainer,
-  LogoSquare1,
-  LogoSquare2,
-  LogoSquare3,
-  LogoSquare4,
-  YouText,
-  FiText,
+  LogoImage,
 } from './YouFiLogo.styles';
+
+const logoSource = require('@/assets/images/youfi-logo.png');
 
 export default function YouFiLogo() {
   return (
     <LogoContainer>
-      <LogoSquareContainer>
-        <LogoSquare1>
-          <LinearGradient
-            colors={['#cef1fc', '#2ccbff']}
-            style={{
-              width: '100%',
-              height: '100%',
-              borderRadius: 6,
-              transform: [{ rotate: '314.314deg' }],
-            }}
-          />
-        </LogoSquare1>
-        <LogoSquare2>
-          <LinearGradient
-            colors={['#cef1fc', '#2ccbff']}
-            style={{
-              width: '100%',
-              height: '100%',
-              borderRadius: 6,
-              transform: [{ rotate: '314.314deg' }],
-            }}
-          />
-        </LogoSquare2>
-        <LogoSquare3>
-          <LinearGradient
-            colors={['#cef1fc', '#2ccbff']}
-            style={{
-              width: '100%',
-              height: '100%',
-              borderRadius: 6,
-              transform: [{ rotate: '314.314deg' }],
-            }}
-          />
-        </LogoSquare3>
-        <LogoSquare4>
-          <LinearGradient
-            colors={['#cef1fc', '#2ccbff']}
-            style={{
-              width: '100%',
-              height: '100%',
-              borderRadius: 4.8,
-              transform: [{ rotate: '314.314deg' }],
-            }}
-          />
-        </LogoSquare4>
-      </LogoSquareContainer>
-      <YouText>You</YouText>
-      <FiText>Fi</FiText>
+      <LogoImage source={logoSource} />
     </LogoContainer>
   );
 }
